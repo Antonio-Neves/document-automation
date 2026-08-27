7fc84efec285ca74ca87a203539978e5be91dd6e
Antonio-Neves <antoniocalheirosneves@gmail.com>
2026-08-27 16:59:12 -0300

    Added static and media paths

--- config/settings.py
+++ config/settings.py
@@ -171,11 +171,16 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 # ----------------------------------------------------------
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # ----------------------------------------------------------
 # Default primary key field type
